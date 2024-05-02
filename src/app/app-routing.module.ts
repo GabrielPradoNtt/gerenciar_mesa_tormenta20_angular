518f836009a33e339297a761393805876780e593
Gabriel Prado <gabriel.prado@everis.com>
2024-05-02 17:10:49 -0300

    criar componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,64 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { CharactersComponent } from './page/characters/characters.component';
+import { SheetComponent } from './page/characters/sheet/sheet.component';
+import { ConfigComponent } from './page/config/config.component';
+import { GroupComponent } from './page/group/group.component';
+import { HomeComponent } from './page/home/home.component';
+import { NpcsComponent } from './page/npcs/npcs.component';
+import { PostComponent } from './page/post/post.component';
+import { StoryComponent } from './page/story/story.component';
+import { WorldComponent } from './page/story/world/world.component';
+import { ThreatsComponent } from './page/threats/threats.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'posts',
+    component: PostComponent
+  },
+  {
+    path: 'CharactersComponent',
+    component: CharactersComponent
+  },
+  {
+    path: 'CharactersComponent/SheetComponent',
+    component: SheetComponent
+  },
+  {
+    path: 'GroupComponent',
+    component: GroupComponent
+  },
+  {
+    path: 'NpcsComponent',
+    component: NpcsComponent
+  },
+  {
+    path: 'ThreatsComponent',
+    component: ThreatsComponent
+  },
+  {
+    path: 'StoryComponent',
+    component: StoryComponent
+  },
+  {
+    path: 'StoryComponent/WorldComponent',
+    component: WorldComponent
+  },
+  {
+    path: 'ConfigComponent',
+    component: ConfigComponent
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './core/nav-bar/nav-bar.component';
+import { HomeComponent } from './page/home/home.component';
+import { PostComponent } from './page/post/post.component';
+import { CharactersComponent } from './page/characters/characters.component';
+import { SheetComponent } from './page/characters/sheet/sheet.component';
+import { GroupComponent } from './page/group/group.component';
+import { NpcsComponent } from './page/npcs/npcs.component';
+import { ThreatsComponent } from './page/threats/threats.component';
+import { StoryComponent } from './page/story/story.component';
+import { WorldComponent } from './page/story/world/world.component';
+import { ConfigComponent } from './page/config/config.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    PostComponent,
+    CharactersComponent,
+    SheetComponent,
+    GroupComponent,
+    NpcsComponent,
+    ThreatsComponent,
+    StoryComponent,
+    WorldComponent,
+    ConfigComponent
   ],
   imports: [
     BrowserModule,
