b00d9eea42dddb393f03a929c2048910300ce09e
GabrielPradoNtt <gabprado@nttdata.com>
2024-07-22 12:43:36 -0300

    restart angular to v18

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  standalone: true,
+  imports: [],
+  template: `
+    <h1>Default</h1>
+  `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'gerenciar_mesa_tormenta20_angular';
+  title = 'default';
 }
