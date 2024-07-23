745de2bd05e3ebb08eaf7647ce93d6aa120973d1
GabrielPradoNtt <gabprado@nttdata.com>
2024-07-23 11:22:10 -0300

    update app menu component

--- src/app/core/menu/botoes-menu/botoes-menu.component.ts
+++ src/app/core/menu/botoes-menu/botoes-menu.component.ts
@@ -1,10 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-botoes-menu',
   standalone: true,
   imports: [
+    CommonModule,
     MatIconModule
   ],
   templateUrl: './botoes-menu.component.html',
@@ -14,4 +17,10 @@ export class BotoesMenuComponent {
   @Input() nome = '';
   @Input() link = '';
   @Input() icone = '';
+  @Input() ativo = false;
+
+  constructor(private router: Router) {}
+  navigateTo() {
+    this.router.navigate([this.link]);
+  }
 }
--- src/app/core/menu/menu.component.ts
+++ src/app/core/menu/menu.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {CommonModule} from '@angular/common';
 import { BotoesMenuComponent } from './botoes-menu/botoes-menu.component';
 import { MatIconModule } from '@angular/material/icon';
-import { ActivatedRoute, Router, RouterState } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRoute, NavigationEnd, Router, RouterState } from '@angular/router';
+import { filter, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-menu',
@@ -16,17 +16,56 @@ import { Observable } from 'rxjs';
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
   listaBotoes = [
-    { nome: 'Tormenta 20', link: '/', icone:'castle'},
-    { nome: 'Jogadores', link: '/jogadores', icone:'person'},
-    { nome: 'Grupo', link: '/grupos', icone:'groups'},
-    { nome: 'Não Jogáveis (NPC)', link: '/npc', icone:'diversity_3'},
-    { nome: 'Ameaças', link: '/ameacas', icone:'sports_mma'},
-    { nome: 'Historias', link: '/historias', icone:'menu_book'},
-    { nome: 'Config.', link: '/config', icone:'settings'}
+    { id: "home", nome: 'Tormenta 20', link: '/', icone:'castle'},
+    { id: "jogador", nome: 'Jogadores', link: '/jogadores', icone:'person'},
+    { id: "grupo", nome: 'Grupo', link: '/grupos', icone:'groups'},
+    { id: "npc", nome: 'Não Jogáveis (NPC)', link: '/npc', icone:'diversity_3'},
+    { id: "ameaca", nome: 'Ameaças', link: '/ameacas', icone:'sports_mma'},
+    { id: "historia", nome: 'Historias', link: '/historias', icone:'menu_book'},
+    { id: "config", nome: 'Config.', link: '/config', icone:'settings'}
   ];
+  protected subRoute: string | null = null;
+  private routerSubscription: Subscription | null = null;
 
-  constructor() {}
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.routerSubscription = this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        const path = this.router.routerState.snapshot.url;
+        this.setSubRoute(path);
+      });
+
+    const initialPath = this.route.snapshot.url.map(segment => segment.path).join('/');
+    this.setSubRoute(initialPath);
+  }
+
+  ngOnDestroy(): void {
+    if (this.routerSubscription) {
+      this.routerSubscription.unsubscribe();
+    }
+  }
+
+  navigatePesquisa() {
+    this.router.navigate(['/pesquisar']);
+  }
+
+  setSubRoute(path: string) {
+    if( !!path && path !== '/' ) {
+      [
+        { id: "pesquisar", link: '/pesquisar'}, 
+        ...this.listaBotoes
+      ].forEach(button => {
+        if(path.includes(button.link) && button.id !== 'home') {
+          this.subRoute = button.id;
+        }
+      })
+    } else {
+      this.subRoute = 'home';
+    }
+  }
 
 }
